3796dcb18175ca7b4f431c85add5a6f69f669e5a
sailyoung97 <sailyoung97@users.noreply.github.com>
2026-08-03 15:58:12 +0800

    add Google HTML verification file

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,7 @@ export const metadata: Metadata = {
     other: {
       "baidu-site-verification": "codeva-SMiXaUr3PP",
       "360-site-verification": "6d3d56c88ca4d6f8df05d09ef1b0a8ed",
-      "msvalidate.01": "0638C467888EF2C5CB3D48BE54D402FE",
-      "google-site-verification": "googleb513e317d6b85915.html"
+      "msvalidate.01": "0638C467888EF2C5CB3D48BE54D402FE"
     }
   },
   openGraph: {
